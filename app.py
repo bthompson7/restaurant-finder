4dd5121ecd559e20f86454ab443f8f0dd9d2645e
Ben Thompson <benjamin.philip.thompson@maine.edu>
2020-05-16 18:43:41 -0400

    Cleaned it up hopefully it works

--- app.py
+++ app.py
@@ -25,9 +25,8 @@ app = Flask(__name__)
 app.debug = True
 API_KEY = os.environ['API_KEY']
 
-print("Starting")
-restList = []
-
+print("Starting Up")
+MAX_RESULTS = 50
 
 
 @app.errorhandler(404)
@@ -38,32 +37,6 @@ def page_not_found(e):
 def main():
     return render_template('index.html')
     
-@app.route('/findlocation', methods=['GET','POST'])
-def postmethod():
-    global data
-    data = request.get_json()
-    print("/findlocation called")
-    print(data)
-    return data
-
-@app.route('/rest', methods=['GET','POST'])
-def restTypeMethod():
-    print("/rest called")
-    global foodtype,rest
-    foodtype = None
-    rest = request.get_json()
-    print(rest)
-    return rest
-
-@app.route('/foodtype', methods=['GET','POST'])
-def foodTypeMethod():
-    print("/foodtype called")
-    global foodtype,rest
-    rest = None
-    foodtype = request.get_json()
-    return foodtype
-
-
 @app.route('/restaurant/<string:id>', methods=['GET'])
 def displayRestaurantDetails(id):
     someData = id
@@ -78,28 +51,26 @@ def displayRestaurantDetails(id):
     return render_template('details.html',**locals())
 
 
-@app.route('/getdata', methods=['GET'])
+#example of the data we will get from index.html
+#{'location': {'lat': 43.740220699999995, 'lng': -70.4526554}, 'data': 'chinese', 'type': 'food'}
+#{'location': {'lat': 43.740220699999995, 'lng': -70.4526554}, 'data': 'lunch', 'type': 'rest'}
+@app.route('/getdata', methods=['GET','POST'])
 def getdata():
-    print("getdata called")
+    restData = request.get_json()
+    print("/getdata called")
+    #print(restData)
     api = Yelp
-    global data,rest,restList
-    print("data is",data)
-    lat = data['location']['lat']
-    lng = data['location']['lng']
-   
-    if rest is not None:
-        print("Rest is not None")
-        restType = rest['restType']
-        print("restType is",restType)
-        dataFromApi = api.search_nearby(50,restType,lat,lng)
-    elif rest is None:
-        print("restType is",rest)
-        print("Food type is ",foodtype)
-        food = foodtype['foodType']
-        dataFromApi = api.search_nearby_for_type(50,lat,lng,food)
-        print("Rest is None")
-        
-    rest = None
+    lat = restData['location']['lat']
+    lng = restData['location']['lng']
+
+    if restData['type'] == 'rest': #rest means breakfast, lunch,dinner
+        restType = restData['data']
+        dataFromApi = api.search_nearby(MAX_RESULTS,restType,lat,lng)
+        print(dataFromApi)
+    elif restData['type'] == 'food': #a specific food type like chinese,burgers etc...
+        food = restData['data']
+        dataFromApi = api.search_nearby_for_type(MAX_RESULTS,lat,lng,food)
+        print(dataFromApi)
     json.dumps(dataFromApi)
     restList = []
     for nearby_restaurant in dataFromApi['businesses']:
